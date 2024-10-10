5741cbb0e9dc3b14824a47e80e50b82650318208
DMG <daniel.marce@mataro.epiaedu.cat>
2024-10-10 21:55:32 +0200

    fix: change exercises statements

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
 // EXERCISE 1: Selecting Elements
-// Select the element with the ID 'header' and log it to the console.
-let element1 = document.querySelector('#header');
+// TODO: Select the element with the ID 'mainTitle' and log it to the console.
+let element1 = document.querySelector('#mainTitle');
 console.log(element1);
 
-// EXERCISE 2: Changing HTML Content
-// Change the inner HTML of the element with the ID 'main-content' to contain a <p> tag with the text "Updated Content".
-let element2 = document.querySelector('#main-content');
+// Exercise 2: Changing HTML Content
+// TODO: Select all paragraph elements and change the content of the second paragraph to "This paragraph has been changed."
+let element2 = document.querySelector('#content');
 element2.innerHTML = '<p>Updated Content</p>';
 
-// EXERCISE 3: Changing CSS Styles
-// Change the background color of the element with the class 'container' to 'lightgreen'.
-let element3 = document.querySelector('.container');
+/// Exercise 3: Changing CSS Styles
+// TODO: Select the first list item and change its text color to red
+let element3 = document.querySelector('#content');
 element3.style.backgroundColor = 'lightgreen';
 
-// EXERCISE 4: Adding/Removing Classes
-// Add a new class called 'active' to the element with the class 'menu'.
-// After 2 seconds, remove the class 'active' from the same element.
-let element4 = document.querySelector('.menu');
+// Exercise 4: Adding/Removing Classes
+// TODO: Add the "highlight" class to the first paragraph
+// TODO: Remove the "highlight" class from the first paragraph after 3 seconds
+let element4 = document.querySelector('#list');
 element4.classList.add('active');
 setTimeout(function() {
     element4.classList.remove('active');
 }, 2000);
 
-// EXERCISE 5: Creating and Appending Elements
-// Create a new <button> element with the text 'Click Me'.
-// Append this button to the element with the ID 'button-container'.
+// Exercise 5: Creating and Appending Elements
+// TODO: Create a new list item with the text "Item 4" and append it to the existing list
 let newButton = document.createElement('button');
-let element5 = document.querySelector('#button-container');
+let element5 = document.querySelector('#content');
 newButton.textContent = 'Click Me';
 element5.appendChild(newButton);
 
-// EXERCISE 6: Removing Elements
-// Select the element with the class 'advertisement' and remove it from the DOM.
-let element6 = document.querySelector('.advertisement');
+// Exercise 6: Removing Elements
+// TODO: Remove the last list item from the unordered list
+let element6 = document.querySelector('#result');
 element6.remove();
 
-// EXERCISE 7: Handling Events
-// Add a 'click' event listener to the button created in Exercise 5.
-// When clicked, the button should alert "Button Clicked!".
+// Exercise 7: Handling Events
+// TODO: Add a click event listener to the button that displays an alert saying "Button clicked!"
 newButton.addEventListener('click', function() {
     alert('Button Clicked!');
 });
